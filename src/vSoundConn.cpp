aab7a2adef714a22eac041a6f2ef8f56609ad4b4
Tristan Matthews <tristan@sat.qc.ca>
2011-01-12 16:13:15 -0500

    vSoundConn: use initializer list

--- src/vSoundConn.cpp
+++ src/vSoundConn.cpp
@@ -3,22 +3,22 @@
 
 // *****************************************************************************
 
-vSoundConn::vSoundConn(vBaseNode *src, vBaseNode *snk)
+vSoundConn::vSoundConn(vBaseNode *src, vBaseNode *snk) : 
+    src_(src), 
+    snk_(snk), 
+    distanceEffect_(100.0), 
+    rolloffEffect_(100.0),
+    dopplerEffect_(100.0),
+    diffractionEffect_(100.0)
 {
-	id_ = src->id_ + "-" + snk->id_ + ".conn";
-	this->src_ = src;
-	this->snk_ = snk;
-	distanceEffect_ = 100.0;
-	rolloffEffect_ = 100.0;
-	dopplerEffect_ = 100.0;
-	diffractionEffect_ = 100.0;
-	
-	// set updateFlag on at least one of the nodes for initial computation:
-	src->updateFlag_ = true;
+    id_ = src->id_ + "-" + snk->id_ + ".conn";
+
+    // set updateFlag on at least one of the nodes for initial computation:
+    src->updateFlag_ = true;
 }
 
 
 vSoundConn::~vSoundConn()
 {
-	// destructor
+    // destructor
 }
