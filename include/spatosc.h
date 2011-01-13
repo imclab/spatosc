821a217b7944b7ff5d048728de57f428426e9087
Alexandre Quessy <alexandre@quessy.net>
2011-01-13 12:19:38 -0500

    adding spatosc.h

---
+++ include/spatosc.h
@@ -0,0 +1,39 @@
+/*
+ * This file is part of Spatosc.
+ * 
+ * Copyright (c) 2010 Society for Arts and Technologies <info@sat.qc.ca>
+ * 
+ * Spatosc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Spatosc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Spatosc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Single public header file for spatosc.
+ * 
+ * Includes the other header files.
+ */
+#ifndef __SPATOSC_H__
+#define __SPATOSC_H__
+
+#include "OSCutil.h"
+#include "vAudioManager.h"
+#include "vBaseNode.h"
+#include "vListener.h"
+#include "vMath.h"
+#include "vPlugin_dmitri.h"
+#include "vPlugin.h"
+#include "vSoundConn.h"
+#include "vSoundSource.h"
+
+#endif
+
