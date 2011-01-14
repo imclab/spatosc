fc6770a9788b2171c4ef94eae598c2520c1504a9
Alexandre Quessy <alexandre@quessy.net>
2011-01-14 17:25:11 -0500

    faster, please

--- tests/check_it_works.cpp
+++ tests/check_it_works.cpp
@@ -62,7 +62,7 @@ int main(int /*argc*/, char ** /*argv*/)
     foo->setPosition(0,10,0);
     bar->setPosition(5,5,0);
 
-    sleep(1);
+    sleep(0.1);
 
     foo->setPosition(0,5,0);
     bar->setPosition(-5,5,0);
