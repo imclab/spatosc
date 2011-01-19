2b0f18915099c7a72e0adf9d9df75dc9e2561b3d
Alexandre Quessy <alexandre@quessy.net>
2011-01-19 12:20:37 -0500

    updated license info for spatosc example

--- examples/openalspat/audio_scene.cpp
+++ examples/openalspat/audio_scene.cpp
@@ -1,22 +1,21 @@
-
-// AudioScene.cpp
-//
-// Copyright (c) 2010 Tristan Matthews <le.businessman@gmail.com>
-//
-// This file is part of spatosc.
-//
-// spatosc is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// spatosc is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with spatosc.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * This file is part of spatosc.
+ *
+ * Copyright (c) 2010 Society for Arts and Technology <info@sat.qc.ca>
+ *
+ * spatosc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * spatosc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with spatosc.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "audio_scene.h"
 #include <stdexcept>
@@ -90,8 +89,7 @@ AudioScene::~AudioScene()
 
 void AudioScene::updatePosition()
 {
-    alSourcefv(source_, AL_POSITION,
-            sourcePos_);
+    alSourcefv(source_, AL_POSITION, sourcePos_);
 }
 
 void AudioScene::moveSourceRaise()
