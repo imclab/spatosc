286b060f79fc5db1a1123ec2af5078a54033d412
Alexandre Quessy <alexandre@quessy.net>
2011-01-19 12:09:24 -0500

    added a grid in clutterspat

--- examples/clutterspat/main.cpp
+++ examples/clutterspat/main.cpp
@@ -22,6 +22,42 @@ struct ExampleApplication
 };
 
 /**
+ * Draws a grid.
+ *
+ * The parent must have a size.
+ * Note that it adds it to its parent.
+ */
+ClutterActor *create_grid(ClutterContainer *parent, gfloat interval_x, gfloat interval_y, ClutterColor *color)
+{
+    // group
+    ClutterActor *group = clutter_group_new();
+    clutter_container_add_actor(CLUTTER_CONTAINER(parent), CLUTTER_ACTOR(group));
+    gfloat parent_w = clutter_actor_get_width(CLUTTER_ACTOR(parent));
+    gfloat parent_h = clutter_actor_get_height(CLUTTER_ACTOR(parent));
+
+    gfloat x, y;
+    ClutterActor *rect = NULL;
+    // vertical lines:
+    for (x = 0.0; x < parent_w; x += interval_x)
+    {
+        rect = clutter_rectangle_new_with_color(color);
+        clutter_container_add_actor(CLUTTER_CONTAINER(group), CLUTTER_ACTOR(rect));
+        clutter_actor_set_height(rect, parent_h);
+        clutter_actor_set_width(rect, 1.0);
+        clutter_actor_set_position(rect, x, 0.0);
+    }
+    for (y = 0.0; y < parent_h; y += interval_y)
+    {
+        rect = clutter_rectangle_new_with_color(color);
+        clutter_container_add_actor(CLUTTER_CONTAINER(group), CLUTTER_ACTOR(rect));
+        clutter_actor_set_height(rect, 1.0);
+        clutter_actor_set_width(rect, parent_w);
+        clutter_actor_set_position(rect, 0.0, y);
+    }
+    return group;
+}
+
+/**
  * We override the paint method of a rectangle to actually paint a circle.
  *
  * We use its original color, but with an alpha of 100%.
@@ -151,6 +187,7 @@ int main(int argc, char *argv[])
     Scene scene;
     ClutterActor *stage = NULL;
     ClutterColor black = { 0x00, 0x00, 0x00, 0xff };
+    ClutterColor grid_color = { 0xff, 0xff, 0xff, 0x66 };
     ClutterColor orange = { 0xff, 0xcc, 0x33, 0x00 }; /* transparent orange */
     ExampleApplication app;
 
@@ -159,6 +196,8 @@ int main(int argc, char *argv[])
     clutter_stage_set_color(CLUTTER_STAGE(stage), &black);
     clutter_stage_set_title(CLUTTER_STAGE(stage), WINDOW_TITLE);
     clutter_actor_set_size(stage, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    create_grid(CLUTTER_CONTAINER(stage), 10.0f, 10.0f, &grid_color);
 
     app.foo_actor = clutter_rectangle_new_with_color(&orange);
     g_signal_connect(app.foo_actor, "paint", G_CALLBACK(paint_circle), NULL);
