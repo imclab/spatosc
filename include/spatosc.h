da95ad856a42bc034d34f94f1a1faf5110b52fed
Alexandre Quessy <alexandre@quessy.net>
2011-01-19 23:13:39 -0500

    more doc

--- include/spatosc.h
+++ include/spatosc.h
@@ -25,7 +25,6 @@
 #ifndef __SPATOSC_H__
 #define __SPATOSC_H__
 
-
 /**
  * @namespace spatosc
  * The whole library is within this single namespace.
@@ -96,6 +95,27 @@
  * Next, one should create some SoundSource nodes so that the Listener node can hear some spatialized sound. 
  *
  * Move those nodes around using the methods of the Node class.
+ *
+ * @section License
+ * 
+ * Copyright (c) 2010 Society for Arts and Technologies <info@sat.qc.ca>
+ * 
+ * Spatosc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Spatosc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Spatosc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Mike Wozniewski <mike@mikewoz.com>
+ * @author Tristan Matthews <le.businessman@gmail.com>
+ * @author Alexandre Quessy <alexandre@quessy.net>
  */
 
 #endif // __SPATOSC_H__
