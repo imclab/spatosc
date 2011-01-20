8b48e4b267e1224c5bdaef265cf02b60b7e70554
Alexandre Quessy <alexandre@quessy.net>
2011-01-19 23:00:31 -0500

    documented the mainpage

--- include/spatosc.h
+++ include/spatosc.h
@@ -25,6 +25,7 @@
 #ifndef __SPATOSC_H__
 #define __SPATOSC_H__
 
+
 /**
  * @namespace spatosc
  * The spatosc library provides a set of classes and functions to control different audio spatialization systems via the OpenSoundControl protocol with a single unified interface.
@@ -42,5 +43,47 @@
 #include "connection.h"
 #include "soundsource.h"
 #include "version.h"
+
+/**
+ * @mainpage The spatosc library.
+ *
+ * @section Installation
+ *
+ * Include the header:
+ *
+   \code
+   #include <spatosc/spatosc.h>
+   \endcode
+ *
+ * (You may include individual headers, such as <spatosc/node.h> for example, but they are all included with the common one.)
+ *
+ * If your source file is main.cpp, you can compile it with: 
+ *
+   \verbatim
+   g++ `pkg-config --libs --cflags spatosc-0.1`  -o main main.cpp
+   \endverbatim
+ *
+ * Alternatively, if using autoconf, use the following in configure.ac:
+ * 
+   \verbatim
+   PKG_CHECK_MODULES([SPATOSC], [spatosc-0.1])
+   \endverbatim
+ * 
+ * Then use the generated SPATOSC_CFLAGS and SPATOSC_LIBS variables in the project Makefile.am files. For example:
+ * 
+   \verbatim
+   program_CPPFLAGS = $(SPATOSC_CFLAGS)
+   program_LDADD = $(SPATOSC_LIBS)
+   \endverbatim
+ * 
+ * @section Usage
+ *
+ * One should create a instance of Scene, and set the Translator pluging to use. The Translator takes care of 
+ * sending messages to some audio renderer to render the scene.
+ *
+ * Next, one should create some SoundSource nodes so that the Listener node can hear some spatialized sound. 
+ *
+ * Move those nodes around using the methods of the Node class.
+ */
 
 #endif // __SPATOSC_H__
