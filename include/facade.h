cad38cb6c3e69c0dadb9ab7fcf1140cba7fcdc5f
Alexandre Quessy <alexandre@quessy.net>
2011-01-24 12:25:08 -0500

    added facade.{cpp,h}

---
+++ include/facade.h
@@ -0,0 +1,46 @@
+/*
+ * This file is part of Spatosc.
+ * 
+ * Copyright (c) 2010 Society for Arts and Technologies <info@sat.qc.ca>
+ * 
+ * Spatosc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Spatosc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Spatosc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ * The Facade class. 
+ */
+
+#ifndef __FACADE_H__
+#define __FACADE_H__
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <tr1/memory>
+
+namespace spatosc
+{
+
+/**
+ * Wraps the whole spatosc library.
+ */
+class Facade
+{
+    public:
+    private:
+};
+
+} // end of namespace spatosc
+
+#endif
