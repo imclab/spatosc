0d76a4896bb40169e1d1d7aa70946e86e75a212a
Tristan Matthews <tristan@sat.qc.ca>
2011-01-25 12:31:00 -0500

    oscsender: allow user to specify server port

--- include/oscsender.h
+++ include/oscsender.h
@@ -17,10 +17,17 @@ class OscSender
 {
     public:
         OscSender();
-        OscSender(const std::string &host, const std::string &port);
+        /**
+         * Constructor that will create server on fromPort for sending messages to host:toPort.
+         */
+        OscSender(const std::string &host, const std::string &toPort, const std::string &fromPort);
+        /**
+         * Constructor that will create a random server for sending messages to host:toPort.
+         */
+        OscSender(const std::string &host, const std::string &toPort);
         std::string toString() const;
         const char * host() { return host_.c_str(); }
-        const char * port() { return port_.c_str(); }
+        const char * port() { return toPort_.c_str(); }
          /**
           * Send a message with one or more string, float and/or int args. 
           * The list of arguments MUST be terminated with SPATOSC_ARGS_END.
@@ -34,8 +41,10 @@ class OscSender
         void sendMessage(const std::string &OSCpath, const char *types, va_list ap) const;
         void sendMessage(const std::string &OSCpath, lo_message msg) const;
         std::string host_;
-        std::string port_;
+        std::string toPort_;
+        std::string fromPort_;
         lo_address address_;
+        lo_server server_;
 };
 
 } // end of namespace spatosc
