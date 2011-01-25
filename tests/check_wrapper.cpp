f25048afc2bd805b67a343ea4c920e8ad771c98a
Alexandre Quessy <alexandre@quessy.net>
2011-01-25 13:00:20 -0500

    added check_wrapper test. (does nothing yet)

---
+++ tests/check_wrapper.cpp
@@ -0,0 +1,40 @@
+/*
+ * This file is part of Spatosc.
+ * 
+ * Copyright (c) 2010 Society for Arts and Technologies <info@sat.qc.ca>
+ * 
+ * Spatosc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Spatosc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Spatosc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Tests that the Wrapper works.
+ */
+
+#include <iostream>
+#include "spatosc.h"
+
+using namespace spatosc;
+
+bool test_wrapper()
+{
+    return true;
+}
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+    if (! test_wrapper())
+        return 1;
+    return 0;
+}
+
