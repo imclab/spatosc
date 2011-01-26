7aa0729f80768a97e120fea33b0aff8a54f4e4df
Alexandre Quessy <alexandre@quessy.net>
2011-01-26 10:39:50 -0500

    updated win32_cross_compile script

--- utils/win32_cross_compile.sh
+++ utils/win32_cross_compile.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-export CC=i586-mingw32msvc-gcc
-export CXX=i586-mingw32msvc-c++
-export LD=i586-mingw32msvc-ld
-export AR=i586-mingw32msvc-ar
-export AS=i586-mingw32msvc-as
-export NM=i586-mingw32msvc-nm
-export STRIP=i586-mingw32msvc-strip
-export RANLIB=i586-mingw32msvc-ranlib
-export DLLTOOL=i586-mingw32msvc-dlltool
-export OBJDUMP=i586-mingw32msvc-objdump
-export RESCOMP=i586-mingw32msvc-windres
+# export CC=i586-mingw32msvc-gcc
+# export CXX=i586-mingw32msvc-c++
+# export LD=i586-mingw32msvc-ld
+# export AR=i586-mingw32msvc-ar
+# export AS=i586-mingw32msvc-as
+# export NM=i586-mingw32msvc-nm
+# export STRIP=i586-mingw32msvc-strip
+# export RANLIB=i586-mingw32msvc-ranlib
+# export DLLTOOL=i586-mingw32msvc-dlltool
+# export OBJDUMP=i586-mingw32msvc-objdump
+# export RESCOMP=i586-mingw32msvc-windres
 
+# ./configure --build=i686-pc-linux-gnu --host=i586-mingw32msvc --prefix=/usr/i586-mingw32msvc
+
+make distclean 
 ./configure --host=i586-mingw32msvc --prefix=/usr/i586-mingw32msvc
+make
+
 
