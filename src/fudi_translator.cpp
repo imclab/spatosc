6a0b848c54660415d28335b9e2ccb480219a146c
Alexandre Quessy <alexandre@quessy.net>
2011-02-28 17:12:46 -0500

    FUDI sender works

--- src/fudi_translator.cpp
+++ src/fudi_translator.cpp
@@ -5,9 +5,18 @@
 /* the "pdsend" command.  This is a standalone program that forwards messages
 from its standard input to Pd via the netsend/netreceive ("FUDI") protocol. */
 
+
+/**
+ * @file
+ * The FudiSender class
+ * 
+ * g++ -Wall -o send_fudi fudi_translator.cpp
+ */
+
 #include <sys/types.h>
-#include <string.h>
-#include <stdio.h>
+#include <cstring>
+#include <cstdio>
+#include <string>
 #include <errno.h>
 #include <stdlib.h>
 #ifdef MSW
@@ -22,125 +31,110 @@ from its standard input to Pd via the netsend/netreceive ("FUDI") protocol. */
 #define SOCKET_ERROR -1
 #endif
 
-void sockerror(char *s);
-void x_closesocket(int fd);
+static void x_closesocket(int fd);
 #define BUFSIZE 4096
+#define UNUSED(x) ((void) (x))
 
-int main(int argc, char **argv)
+class FudiSender
+{
+    public:
+        FudiSender(std::string host, unsigned int port, bool isTcp) :
+            host_(host),
+            port_(port),
+            protocol_(0)
+        {
+            if (isTcp)
+                protocol_ = SOCK_STREAM;
+            else
+                protocol_ = SOCK_DGRAM;
+        }
+        bool sendFudi(const std::string &message);
+    private:
+        std::string host_;
+        unsigned int port_;
+        int protocol_;
+        void sockerror(char *s);
+};
+
+bool FudiSender::sendFudi(const std::string &message)
 {
     int sockfd, portno, protocol;
     struct sockaddr_in server;
     struct hostent *hp;
-    char *hostname;
-    int nretry = 10;
+    const char *hostname;
 #ifdef MSW
     short version = MAKEWORD(2, 0);
     WSADATA nobby;
 #endif
-    if (argc < 2 || sscanf(argv[1], "%d", &portno) < 1 || portno <= 0)
-        goto usage;
-    if (argc >= 3)
-        hostname = argv[2];
-    else hostname = "127.0.0.1";
-    if (argc >= 4)
-    {
-        if (!strcmp(argv[3], "tcp"))
-            protocol = SOCK_STREAM;
-        else if (!strcmp(argv[3], "udp"))
-            protocol = SOCK_DGRAM;
-        else goto usage;
-    }
-    else protocol = SOCK_STREAM;
+    hostname = host_.c_str();
+    protocol = protocol_;
+    portno = (int) port_;
 #ifdef MSW
-    if (WSAStartup(version, &nobby)) sockerror("WSAstartup");
+    if (WSAStartup(version, &nobby))
+        sockerror((char *) "WSAstartup");
 #endif
 
     sockfd = socket(AF_INET, protocol, 0);
     if (sockfd < 0)
     {
-        sockerror("socket()");
-        exit(1);
+        sockerror((char *) "socket()");
+        return false;
     }
-        /* connect socket using hostname provided in command line */
+    /* connect socket using hostname provided in command line */
     server.sin_family = AF_INET;
     hp = gethostbyname(hostname);
     if (hp == 0)
     {
         fprintf(stderr, "%s: unknown host\n", hostname);
         x_closesocket(sockfd);
-        exit(1);
+        return false;
     }
-    memcpy((char *)&server.sin_addr, (char *)hp->h_addr, hp->h_length);
+    memcpy((char *) &server.sin_addr, (char *) hp->h_addr, hp->h_length);
 
-        /* assign client port number */
-    server.sin_port = htons((unsigned short)portno);
+    /* assign client port number */
+    server.sin_port = htons((unsigned short) portno);
 
-#if 0   /* try this again for 4.0; this crashed my RH 6.2 machine!) */
-
-        /* try to connect.  */
-    for (nretry = 0; nretry < (protocol == SOCK_STREAM ? 10 : 1); nretry++)
-    
+    /* try to connect.  */
+    if (connect(sockfd, (struct sockaddr *) &server, sizeof(server)) < 0)
     {
-        if (nretry > 0)
+        sockerror((char *) "connect");
+        x_closesocket(sockfd);
+        return false;
+    }
+    /* now loop reading stdin and sending  it to socket */
+    char buf[BUFSIZE];
+    strncpy(buf, message.c_str(), BUFSIZE);
+    char *bp;
+    int nsent, nsend;
+    nsend = strlen(buf);
+    printf("sending=%s     length=%d\n", buf, nsend);
+    for (bp = buf, nsent = 0; nsent <= nsend;)
+    {
+        int res = send(sockfd, bp, nsend - nsent, 0);
+        if (res < 0)
         {
-            sleep (nretry < 5 ? 1 : 5);
-            fprintf(stderr, "retrying...");
+            sockerror((char *) "send");
+            break;
         }
-        if (connect(sockfd, (struct sockaddr *) &server, sizeof (server)) >= 0)
-            goto connected;
-        sockerror("connect");
+        nsent += res;
+        bp += res;
+        if (nsent >= nsend)
+            break;
+        printf("nsent=%d\n", nsent);
     }
     x_closesocket(sockfd);
-    exit(1);
-connected: ;
-#else
-    /* try to connect.  */
-    if (connect(sockfd, (struct sockaddr *) &server, sizeof (server)) < 0)
-    {
-        sockerror("connect");
-        x_closesocket(sockfd);
-        exit(1);
-    }
-#endif
-        /* now loop reading stdin and sending  it to socket */
-    while (1)
-    {
-        char buf[BUFSIZE], *bp;
-        int nsent, nsend;
-        if (!fgets(buf, BUFSIZE, stdin))
-            break;
-        nsend = strlen(buf);
-        for (bp = buf, nsent = 0; nsent < nsend;)
-        {
-            int res = send(sockfd, bp, nsend-nsent, 0);
-            if (res < 0)
-            {
-                sockerror("send");
-                goto done;
-            }
-            nsent += res;
-            bp += res;
-        }
-    }
-done:
-    if (ferror(stdin))
-        perror("stdin");
-    exit (0);
-usage:
-    fprintf(stderr, "usage: pdsend <portnumber> [host] [udp|tcp]\n");
-    fprintf(stderr, "(default is localhost and tcp)\n");
-    exit(1);
+    return true;
 }
 
-void sockerror(char *s)
+void FudiSender::sockerror(char *s)
 {
 #ifdef MSW
     int err = WSAGetLastError();
-    if (err == 10054) return;
+    if (err == 10054)
+        return;
     else if (err == 10044)
     {
-        fprintf(stderr,
-            "Warning: you might not have TCP/IP \"networking\" turned on\n");
+        fprintf(stderr, "Warning: you might not have TCP/IP \"networking\" turned on\n");
     }
 #else
     int err = errno;
@@ -148,7 +142,7 @@ void sockerror(char *s)
     fprintf(stderr, "%s: %s (%d)\n", s, strerror(err), err);
 }
 
-void x_closesocket(int fd)
+static void x_closesocket(int fd)
 {
 #ifdef MSW
     closesocket(fd);
@@ -156,3 +150,14 @@ void x_closesocket(int fd)
     close(fd);
 #endif
 }
+
+int main(int argc, char *argv[])
+{
+    UNUSED(argc);
+    UNUSED(argv);
+
+    FudiSender sender("localhost", 31337, true);
+    sender.sendFudi("hello 1 2 3.14159;");
+    return 0;
+}
+
