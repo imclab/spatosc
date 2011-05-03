imclab/spatosc

commit 00756d5cbf55972b06f598e6d6f3c18c48685187

fixed bug where only updated for the first connection for a node are sent. the problem was that that the node::stateSent() method was called for each connection rather than for each node

src/scene.cpp
@@ -434,8 +434,11 @@ void Scene::pushConnectionChangesViaAllTranslators(Connection *conn, bool forced
         std::map<std::string, std::tr1::shared_ptr<Translator> >::iterator iter;
         for (iter = translators_.begin(); iter != translators_.end(); ++iter)
             iter->second->pushConnectionChanges(conn);
-        src->stateSent();
-        sink->stateSent();
+
+        // !!! can't do this here, because node may be needed for other
+        // connections.
+        //src->stateSent();
+        //sink->stateSent();
     }
 }
 

src/soundsource.cpp
@@ -56,6 +56,8 @@ void SoundSource::onNodeChanged(bool forcedNotify)
 
     for (c = connectTO_.begin(); c != connectTO_.end(); ++c)
         scene_.onConnectionChanged(c->get(), forcedNotify);
+
+    stateSent();
 }
 
 } // end namespace spatosc

include/node.h
@@ -150,7 +150,7 @@ class Node
         }
 
         /**
-         * Notify this node that it's new position has been sent
+         * Notify this node that it's new data has been sent
          */
         void stateSent()
         {
@@ -234,6 +234,8 @@ class Node
         bool nodeChanged_;
     private:
         Vector3 pos_;
+        // NOTE: onNodeChanged MUST call stateSent(), which unsets the changed
+        // flag
         virtual void onNodeChanged(bool forcedNotify=false) = 0;
         virtual bool handleMessage_(const std::string &method, int argc, lo_arg ** argv, const char *types) = 0;
         Properties<std::string> string_properties_;

src/listener.cpp
@@ -74,5 +74,7 @@ void Listener::onNodeChanged(bool forcedNotify)
 
     for (c = connectFROM_.begin(); c != connectFROM_.end(); ++c)
         scene_.onConnectionChanged(c->get(), forcedNotify);
+
+    stateSent();
 }
 } // end namespace spatosc

src/node.cpp
@@ -131,7 +131,6 @@ bool correctNumberOfArguments(const std::string &method, int expected, int actua
 // FIXME: need to provide the types as well.
 void Node::handleMessage(const std::string &method, int argc, lo_arg **argv, const char *types)
 {
-    UNUSED(types);
     using namespace OSCutil; // typeTagsMatch
     if (method == "setActive")
     {

commit 9a1bdd290c8ca7cf6664cffc097d439b52818c27

fixed asynchonous mode stateSent bug that was introduced in commit 00756d5cbf55972b06f5

include/scene.h
@@ -318,6 +318,8 @@ class Scene
          */
         void setSynchronous(bool synchronous);
 
+        bool isSynchronous() { return synchronous_; }
+
         /**
          * When in asynchronous mode, (not synchronous) one needs to call this quite often to flush the OSC messages.
          * To be in the asynchronous mode, one should setSynchronous with false as parameter.

src/scene.cpp
@@ -451,13 +451,20 @@ bool Scene::flushMessages()
     }
     else
     {
-        ConnConstIterator iter;
-        for (iter = connections_.begin(); iter != connections_.end(); ++iter)
+        ConnConstIterator c;
+        for (c = connections_.begin(); c != connections_.end(); ++c)
         {
-            Connection* conn = (*iter).get();
+            Connection* conn = (*c).get();
             if (conn->active())
                 pushConnectionChangesViaAllTranslators(conn);
         }
+
+        // now reset all changed flags on nodes:
+        for (ListenerIterator L = listeners_.begin(); L != listeners_.end(); ++L)
+            (*L)->stateSent();
+        for (SourceIterator n = soundSources_.begin(); n != soundSources_.end(); ++n)
+            (*n)->stateSent();
+
         return true;
     }
 }

src/soundsource.cpp
@@ -57,7 +57,7 @@ void SoundSource::onNodeChanged(bool forcedNotify)
     for (c = connectTO_.begin(); c != connectTO_.end(); ++c)
         scene_.onConnectionChanged(c->get(), forcedNotify);
 
-    stateSent();
+    if (scene_.isSynchronous()) stateSent();
 }
 
 } // end namespace spatosc

tests/check_asynchronous.cpp
@@ -57,6 +57,13 @@ bool test_async()
     SoundSource *source = scene.createSoundSource("source");
     Listener *listener = scene.createListener("listener");
     scene.connect(source, listener);
+
+    // In synchronous mode, changing a position results in a connection update.
+    // We want it to happen only after flushMessages():
+
+    DummyTranslator::pushed = false;
+    source->setPosition(1.0, 2.0, 3.0);
+
     if (DummyTranslator::pushed)
     {
         std::cerr <<" should not have pushed yet.\n";