e6764b26c35ba07cee7bcc53ff50ca9b92e4ebd7
Zack Settel <zkfax@sympatico.ca>
2011-08-09 18:29:45 -0400

    replaced OSCroute with OSCparse in basic-renderer.pd

--- examples/pd-renderer/spinClient.sh
+++ examples/pd-renderer/spinClient.sh
@@ -49,14 +49,14 @@ killall Pd-extended; killall spinserver; killall spinviewer;
 
 
 
-$SPINSERV &
-$SPINVIEW --screen 1 &
+#$SPINSERV &
+#$SPINVIEW --screen 1 &
 
 echo launch audio renderer
 
-$PDE -send "spatOSC-port 18032" -send "renderer-port 18222" -open basic-renderer.pd  spatOSC-spinClient.pd spinEdit.pd &
+#$PDE -send "spatOSC-port 18032" -send "renderer-port 18222" -open basic-renderer.pd  spatOSC-spinClient.pd spinEdit.pd &
 
-#$PDE -send "spatOSC-port 18032" -send "renderer-port 18222" -open basic-renderer.pd  spatOSC-client.pd &
+$PDE -send "spatOSC-port 18032" -send "renderer-port 18222" -open basic-renderer.pd  spatOSC-client.pd &
 
 #$PDE -send "spatOSC-port 18032" -open basic-renderer.pd -nogui &
 #$PDE -send "spatOSC-port 18032" -open spatOSC-client.pd &
