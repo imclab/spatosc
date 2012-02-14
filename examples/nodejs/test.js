cab0cbfce077f3a05cc8228444e03f7fecf93ed7
Alexandre Quessy <alexandre@quessy.net>
2012-02-14 17:02:26 -0500

    node.js proto: it works

--- examples/nodejs/test.js
+++ examples/nodejs/test.js
@@ -1,7 +1,8 @@
 var addon = require("./hello");
+console.log(addon);
 
-var obj1 = addon.createObject("hello");
-var obj2 = addon.createObject("world");
+//var obj1 = addon.createObject("hello");
+//var obj2 = addon.createObject("world");
 
-console.log(obj1.msg + " " + obj2.msg); // 'hello world'
+console.log("message: " + addon.hello); // + " " + obj2.msg); // 'hello world'
 
