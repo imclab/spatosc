a78a306d0ec91fe8361c0ea6d68a4883e29114bd
Alexandre Quessy <alexandre@quessy.net>
2012-02-16 18:40:56 -0500

    python example rotates a node

--- python_module/example.py
+++ python_module/example.py
@@ -5,16 +5,23 @@ import time
 
 def run():
     scene = spatosc.Wrapper()
+    scene.setVerbose(True)
+    scene.addTranslatorWithAddress("translator0", "BasicTranslator", "osc.udp://127.0.0.1:10000");
     scene.createSource("source0")
     scene.createListener("listener0")
-    scene.setPosition("source0", 1.0, 1.0, 1.0)
+    # scene.setSynchronous(True) # FIXME: does this work at all?
     scene.debugPrint()
+    try:
+        angle = 0.0
+        while True:
+            time.sleep(1.0)
+            angle = (angle + 10.0) % 360.0
+            scene.setPositionAED("source0", angle, 0.0, 10.0)
+            print("scene.setPositionAED(\"%s\", %f, %f, %f)" % ("source0", angle, 0.0, 10.0))
+            scene.flushMessages()
+    except KeyboardInterrupt, e:
+        print("")
 
 if __name__ == "__main__":
     run()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt, e:
-        print("")
 
