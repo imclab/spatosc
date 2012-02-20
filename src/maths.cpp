ca12097da295aa0453f4ff845a4f78fa5055328d
Mike Wozniewski <mike@mikewoz.com>
2012-02-20 16:13:12 -0500

    really fixed the spherical to cartesian function now (it had a bad coordinate system)

--- src/maths.cpp
+++ src/maths.cpp
@@ -389,12 +389,14 @@ Vector3 QuatToEuler(Quaternion q1)
 Vector3 sphericalToCartesian(Vector3 aed)
 {
     // http://en.wikipedia.org/wiki/Spherical_coordinate_system
-    double a = aed.x;
+    //double a = -aed.x-M_PI/2;
+    double a = (aed.x-M_PI/2);
     double e = aed.y-M_PI/2;
     double d = aed.z;
     double x = d * sin(e) * cos(a);
     double y = d * sin(e) * sin(a);
     double z = d * cos(e);
+    //double z = d * cos(a-M_PI/2);
     return Vector3(x,y,z);
 }
 
